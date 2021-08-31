499d687dd96685ac4b2c02164dd3bb5a2809d3f0
nishina555 <n.toshi.1.014@gmail.com>
2021-08-31 20:45:45 +0900

    APIリクエストのStateを定義する

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const REQUEST_STATUS = {
+  NONE: "NONE",
+  REQUEST: "REQUEST",
+  SUCCESS: "SUCCESS",
+  FAILURE: "FAILURE",
+} as const;
--- src/redux/types.ts
+++ src/redux/types.ts
@@ -1,3 +1,5 @@
+import { REQUEST_STATUS } from "../constants";
+
 export interface TodoItem {
   id: number;
   content: string;
@@ -7,6 +9,10 @@ export interface TodoItem {
 export interface TodoState {
   todoItems: Array<TodoItem>;
 }
+
+export type RequestStatus = typeof REQUEST_STATUS[keyof typeof REQUEST_STATUS];
+
+export type RequestState = { status: RequestStatus };
 
 export interface RootState {
   todos: TodoState;
