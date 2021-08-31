c4b3e89cdc70106770a6335f95c9a1982823bcd8
nishina555 <n.toshi.1.014@gmail.com>
2021-08-31 21:33:58 +0900

    EpicミドルウェアをStoreに追加する

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./reducers";
+import rootEpic from "./epics";
+import { createEpicMiddleware } from "redux-observable";
 
+const epicMiddleware = createEpicMiddleware();
 const store = configureStore({
   reducer: rootReducer,
+  middleware: [epicMiddleware],
 });
+
+epicMiddleware.run(rootEpic);
 
 export default store;
