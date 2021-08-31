86a1e326bbf039b6f940d5c34ad1a518ac85c482
nishina555 <n.toshi.1.014@gmail.com>
2021-08-31 21:40:12 +0900

    複数のアクションがEpicで発火するようにする

--- src/redux/epics/todoEpics.ts
+++ src/redux/epics/todoEpics.ts
@@ -1,16 +1,27 @@
 import { Epic, ofType } from "redux-observable";
-import { map, mergeMap } from "rxjs/operators";
+import { mergeMap, concatMap, catchError } from "rxjs/operators";
 import { GetTodosType } from "../actionTypes";
-import { TodoActions, setTodos, GetTodosActions } from "../actions";
+import {
+  setTodos,
+  getTodosSuccess,
+  GetTodosActions,
+  getTodosFailure,
+} from "../actions";
 import axios from "axios";
 import { from } from "rxjs";
+import { AnyAction } from "redux";
+import { of } from "rxjs";
 
-export const getTodosEpic: Epic<GetTodosActions | TodoActions> = (action$) =>
+export const getTodosEpic: Epic<GetTodosActions, AnyAction> = (action$) =>
   action$.pipe(
     ofType(GetTodosType.GET_TODOS_REQUEST),
     mergeMap(() =>
       from(axios.get("http://localhost:4000/todos")).pipe(
-        map((response) => setTodos(response.data))
+        concatMap((response) => [setTodos(response.data), getTodosSuccess()]),
+        catchError((error: Error) => {
+          console.log(error);
+          return of(getTodosFailure());
+        })
       )
     )
   );
